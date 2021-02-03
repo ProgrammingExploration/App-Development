856f9ddc053c0183a0d16b34413b059662865479
littlepotato123 <sopanda0910@gmail.com>
2021-02-03 15:26:47 -0500

    Exploration 13 - Done

---
+++ Exploration 13/code/lib/pages/about.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class About extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return Text('About Page');
+  }
+}
---
+++ Exploration 13/code/lib/pages/stats.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class Stats extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return Text('Stats Page');
+  }
+}
